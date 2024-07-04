30aaaef14332ffc9bda35776bb0eed757dfaa6f3
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-07-05 02:35:52 +0300

    added a debug line to build_literal function

--- cobc/tree.c
+++ cobc/tree.c
@@ -1409,6 +1409,7 @@ build_literal (const enum cb_category category, const void *data,
 		p->size -= outbytesleft;
 
 	}
+	warnx("%s:%d: category %d, data %p, size %zu", __func__, __LINE__, category, data, size);
 	return p;
 }
 
