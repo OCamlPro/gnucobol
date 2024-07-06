c1c3f051a0d4dddb4327d7077c64fbc51ad6f3e3
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-07-06 16:11:24 +0300

    Revert "added a debug line to build_literal function"
    This reverts commit 0fc258ff6558c3339fb9f7d76f84bc83d2d4b7f8.

--- cobc/tree.c
+++ cobc/tree.c
@@ -1409,7 +1409,6 @@ build_literal (const enum cb_category category, const void *data,
 		p->size -= outbytesleft;
 
 	}
-	warnx("%s:%d: category %d, data %p, size %zu", __func__, __LINE__, category, data, size);
 	return p;
 }
 
