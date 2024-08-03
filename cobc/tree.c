91ead8679198384f5dc7f81fefc07d57208640fd
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-08-03 17:00:42 +0300

    created a new function cb_build_gcos_literal and used it with the gcos literals

--- cobc/tree.c
+++ cobc/tree.c
@@ -2789,6 +2789,18 @@ cb_build_literal_by_category(const void *data, const size_t size, int category)
 	return l;
 }
 
+cb_tree
+cb_build_gcos_literal(const void *data, const size_t size)
+{
+	cb_tree			l;
+
+	l = CB_TREE (build_literal (CB_CATEGORY_ALPHANUMERIC, data, size));
+
+	l->source_file = cb_source_file;
+	l->source_line = cb_source_line;
+
+	return l;
+}
 
 cb_tree
 cb_build_alphanumeric_literal (const void *data, const size_t size)
