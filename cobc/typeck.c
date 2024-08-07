f589d0b14e4f482cb29a5da22d379e46312a9a0e
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-08-07 16:13:37 +0300

    refactor typeck.c/validate_move

--- cobc/typeck.c
+++ cobc/typeck.c
@@ -11118,7 +11118,7 @@ validate_move (cb_tree src, cb_tree dst, const unsigned int is_value, int *move_
 	int			dst_size_mod;
 	signed int			size;	/* -1 as special value */
 	int			m_zero;
-	int class;
+	int class = CB_TREE_CLASS(src);
 
 
 	/* CHECKME: most of the "invalid" checks should possibly be handled in the parser */
@@ -11564,7 +11564,6 @@ validate_move (cb_tree src, cb_tree dst, const unsigned int is_value, int *move_
 			 && !fdst->flag_any_length) {
 				/* check the real size */
 				fdst = CB_FIELD_PTR (dst);
-				class = CB_TREE_CLASS(src);
 				i = trimmed_size( l->data, l->size, fdst->flag_justified, class);
 				i /= COB_NATIONAL_SIZE;
 				if( size < i ) {
@@ -11668,7 +11667,6 @@ validate_move (cb_tree src, cb_tree dst, const unsigned int is_value, int *move_
 			 && !fdst->flag_any_length) {
 				/* check the real size */
 				fdst = CB_FIELD_PTR (dst);
-				class = CB_TREE_CLASS(src);
 				i = trimmed_size( l->data, l->size, fdst->flag_justified, class);
 				if( size < i ) {
 					size = (signed int)i;
