e506e35127085ff968f6f6db74b8e118f24e94d6
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-08-09 20:57:39 +0300

    added the command line argument for the source encoding

--- cobc/cobc.c
+++ cobc/cobc.c
@@ -109,6 +109,7 @@ enum compile_level {
 #define	CB_FLAG_GETOPT_MEMORY_CHECK         17
 #define	CB_FLAG_GETOPT_COPY_FILE            18
 #define	CB_FLAG_GETOPT_INCLUDE_FILE         19
+#define CB_FLAG_GETOPT_SOURCE_ENCODE        20
 
 
 /* Info display limits */
@@ -318,6 +319,12 @@ int cb_flag_alt_ebcdic = 0;
 #ifdef HAVE_ICONV_H
 // call the struct cb_iconv
 struct cb_iconv_t cb_iconv;
+
+static void 
+initialize_cb_iconv() {
+	strncpy(cb_iconv.source, "UTF-8", sizeof(cb_iconv.source) - 1);
+    cb_iconv.source[sizeof(cb_iconv.source) - 1] = '\0';
+}
 #endif
 
 
@@ -3852,6 +3859,32 @@ process_command_line (const int argc, char **argv)
 				cb_fold_call = COB_FOLD_LOWER;
 			} else {
 				cobc_err_exit (COBC_INV_PAR, "-ffold-call");
+			}
+			break;
+
+		case CB_FLAG_GETOPT_SOURCE_ENCODE:
+			/* -fsource-encode=encoding*/
+			const char* valid_encodings[] = {
+				"UTF-8",
+				"ASCII",
+				"ISO-8859-1",
+				"ISO-8859-15"
+			};
+			int num_encodings = sizeof(valid_encodings) / sizeof(valid_encodings[0]);
+			int encoding_valid = 0;
+			for (int i = 0; i < num_encodings; i++) {
+				if (strcmp(cob_optarg, valid_encodings[i]) == 0) {
+					encoding_valid = 1;
+					break;
+				}
+			}
+			if (encoding_valid) {
+#ifdef HAVE_ICONV_H
+				strncpy(cb_iconv.source, cob_optarg, sizeof(cb_iconv.source) - 1);
+    			cb_iconv.source[sizeof(cb_iconv.source) - 1] = '\0';
+#endif
+			} else {
+				cobc_err_exit(COBC_INV_PAR, "-fsource-encode");
 			}
 			break;
 
@@ -8998,11 +9031,9 @@ begin_setup_internal_and_compiler_env (void)
 {
 	char			*p;
 	
-	// initialize the iconv struct
+	/* initialize the default source encoding */
 #ifdef HAVE_ICONV_H
-	cb_iconv.alphanumeric = iconv_open("ISO-8859-15", "UTF-8");
-	cb_iconv.national = iconv_open("UTF-16LE", "UTF-8");
-	cb_iconv.utf8 = iconv_open("UTF-8", "ISO-8859-15");
+	initialize_cb_iconv();
 #endif
 	
 	/* register signal handlers from cobc */
@@ -9264,6 +9295,14 @@ main (int argc, char **argv)
 
 	/* Process command line arguments */
 	iargs = process_command_line (argc, argv);
+
+/* initialize the iconv struct after reading the command line*/
+#ifdef HAVE_ICONV_H
+	cb_iconv.alphanumeric = iconv_open("ISO-8859-15", cb_iconv.source);
+	cb_iconv.national = iconv_open("UTF-16LE", cb_iconv.source);
+	cb_iconv.utf8 = iconv_open("UTF-8", cb_iconv.source);
+#endif
+
 
 	if (fatal_startup_error) {
 		cobc_err_msg (_("please check environment variables as noted above"));
