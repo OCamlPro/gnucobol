f1d766e5a04bf493f426627bf2f4eda9cf25c896
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2024-09-12 10:14:42 +0200

    Warn when a collating sequence is ignored for keys of indexed files

--- cobc/tree.c
+++ cobc/tree.c
@@ -4781,6 +4781,21 @@ validate_indexed_key_field (struct cb_file *f, struct cb_field *records,
 			}
 		}
 	}
+
+	/* check collating sequence is not ignored */
+	if (CB_TREE_CLASS (k) != CB_CLASS_ALPHANUMERIC) {
+		cb_tree colseq = (cbak == NULL)
+			? f->collating_sequence_key
+			: cbak->collating_sequence_key;
+		if (colseq == NULL) {
+			colseq = f->collating_sequence;
+		}
+		if (colseq != NULL) {
+			cb_warning_x (COBC_WARN_FILLER, CB_TREE(colseq),
+				      _("COLLATING SEQUENCE is ignored for non-alphanumeric"
+					" key '%s'"), k->name);
+		}
+	}
 }
 
 void
