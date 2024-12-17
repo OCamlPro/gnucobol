3efed41d50fa75f2e6b8024e44cc44f43ad7c906
engboris <boris.eng@ocamlpro.com>
2024-12-17 18:17:56 +0100

    Update Changelogs, minor fix and add tests

--- cobc/codegen.c
+++ cobc/codegen.c
@@ -1832,24 +1832,24 @@ output_gnucobol_defines (const char *formatted_date)
 		current_compile_tm.tm_sec;
 	output_line ("#define  COB_MODULE_TIME\t\t%d", i);
 
-	{
-		struct cb_text_list *l;
-		for (l = cb_include_file_list; l; l = l->next) {
-			if (l->text[0] == '<') {
-				output_line ("#include %s", l->text);
-			} else {
-				output_line ("#include \"%s\"", l->text);
-			}
-		}
-
-		for (l = cb_include_file_list_directive; l; l = l->next) {
-			if (l->text[0] == '<') {
-				output_line ("#include %s", l->text);
-			} else {
-				output_line ("#include \"%s\"", l->text);
-			}
+	struct cb_text_list *l;
+	for (l = cb_include_file_list; l; l = l->next) {
+		if (l->text[0] == '<') {
+			output_line ("#include %s", l->text);
+		} else {
+			output_line ("#include \"%s\"", l->text);
 		}
 	}
+
+	for (l = cb_include_file_list_directive; l; l = l->next) {
+		if (l->text[0] == '<') {
+			output_line ("#include %s", l->text);
+		} else {
+			output_line ("#include \"%s\"", l->text);
+		}
+	}
+
+	cb_include_file_list_directive = NULL;
 }
 
 /* CALL cache */
