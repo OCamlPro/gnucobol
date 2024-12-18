d6aa13e62447af3a9b1a481d15a88b0b25b36860
Emilien Lemaire <emilien.lem@icloud.com>
2024-12-18 16:23:27 +0100

    COMP-X displayed with 3 characters when size is one byte

--- cobc/tree.c
+++ cobc/tree.c
@@ -1461,6 +1461,8 @@ cb_tree_category (cb_tree x)
 					x->category = CB_CATEGORY_DATA_POINTER;
 				} else if (f->usage == CB_USAGE_PROGRAM_POINTER) {
 					x->category = CB_CATEGORY_PROGRAM_POINTER;
+				} else if (f->usage == CB_USAGE_COMP_X) {
+					x->category = CB_CATEGORY_NUMERIC;
 				} else if (f->pic) {
 					x->category = f->pic->category;
 				/* FIXME: Hack for CGI to not abort */
@@ -4135,6 +4137,8 @@ cb_field_size (const cb_tree x)
 		if (f->flag_any_length) {
 			return FIELD_SIZE_UNKNOWN;
 		}
+		if (f->usage == CB_USAGE_COMP_X && f->compx_size > 0)
+			return f->compx_size;
 		return f->size;
 	}
 	case CB_TAG_REFERENCE: {
@@ -4152,6 +4156,8 @@ cb_field_size (const cb_tree x)
 			} else {
 				return FIELD_SIZE_UNKNOWN;
 			}
+		} else if (f->usage == CB_USAGE_COMP_X && f->compx_size > 0) {
+			return f->compx_size;
 		} else if (f->flag_any_length) {
 			return FIELD_SIZE_UNKNOWN;
 		} else {
